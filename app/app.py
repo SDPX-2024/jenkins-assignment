276a8d3c906eddb976edb83b90ab4fa853d55e55
Misterpetchz <petch.t1507@gmail.com>
2025-02-17 20:41:03 +0700

    fix: get code route

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/getcode', methods=['GET'])
 def getcode():
-    return "Let Offer to me pls, Agoda"
+    return "Let Offer to Petch pls, Agoda"
 
 @app.route('/plus/<num1>/<num2>', methods=['GET'])
 def plus(num1, num2):
